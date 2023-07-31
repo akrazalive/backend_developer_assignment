0edded0ccbb18817b689bba6059ba702e1c599e5
Asif Khan <akrazalive@gmail.com>
2023-07-31 22:20:18 +0500

    default values added for form

--- index.php
+++ index.php
@@ -45,13 +45,13 @@
                     <!-- Order section -->
                     <div class="form-group">
                         <label for="amount">Price (amount):</label>
-                        <input type="number" class="form-control" name="amount" required>
+                        <input type="number" class="form-control" name="amount" value="100" required>
                     </div>
 
                     <div class="form-group">
                         <label for="currency">Currency:</label>
                         <select class="form-control" name="currency" required>
-                            <option value="USD">USD</option>
+                            <option value="USD" selected>USD</option>
                             <option value="EUR">EUR</option>
                             <option value="THB">THB</option>
                             <option value="HKD">HKD</option>
@@ -62,26 +62,26 @@
 
                     <div class="form-group">
                         <label for="customer_name">Customer Full name:</label>
-                        <input type="text" class="form-control" name="customer_name" required>
+                        <input type="text" class="form-control" name="customer_name" value="John Doe" required>
                     </div>
 
                     <!-- Payment section -->
                     <div class="form-group">
                         <label for="card_holder_name">Credit card holder name:</label>
-                        <input type="text" class="form-control" name="card_holder_name" required>
+                        <input type="text" class="form-control" name="card_holder_name" value="John Doe" required>
                     </div>
 
                     <div class="form-group">
                         <label for="card_number">Credit card number:</label>
-                        <input type="text" class="form-control" name="card_number" required>
+                        <input type="text" class="form-control" name="card_number" value="4111111111111111" required>
                     </div>
 
                     <div class="form-group">
                         <label>Expiration Date:</label>
                         <div class="d-flex">
                             <select class="form-control mr-2" name="expiration_month" required>
-                                <option value="" disabled selected>Month</option>
-                                <option value="01">January</option>
+                                <option value="" disabled>Month</option>
+                                <option value="01" selected>January</option>
                                 <option value="02">February</option>
                                 <option value="03">March</option>
                                 <option value="04">April</option>
@@ -95,22 +95,13 @@
                                 <option value="12">December</option>
                             </select>
 
-                            <input type="number" class="form-control" name="expiration_year" placeholder="Year" list="year-list" required>
-                            <datalist id="year-list">
-                                <?php
-                                $currentYear = date("Y");
-                                for ($i = 0; $i < 10; $i++) {
-                                    echo '<option>' . $currentYear . '</option>';
-                                    $currentYear++;
-                                }
-                                ?>
-                            </datalist>
+                            <input type="number" class="form-control" name="expiration_year" value="2025" required>
                         </div>
                     </div>
 
                     <div class="form-group">
                         <label for="cvv">Credit card CCV:</label>
-                        <input type="text" class="form-control" name="cvv" required>
+                        <input type="text" class="form-control" name="cvv" value="111" required>
                     </div>
 
                     <button type="submit" class="btn btn-primary">Submit</button>
