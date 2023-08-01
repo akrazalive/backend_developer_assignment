40825aabfd0ca08e842eac7fbf049c9b2538c214
akrazalive <akrazalive@gmail.com>
2023-08-01 11:19:13 +0500

    changed keys

--- paypal_config.php
+++ paypal_config.php
@@ -2,7 +2,7 @@
 // paypal_config.php
 
 // Replace with your sandbox PayPal credentials
-define('PAYPAL_CLIENT_ID', 'ARvy2_atoeUKKk-t7m68wd0exgwXZjoBEo1WPg0YnOmiSfo5Yhfs97ntZ8_nPKeVzwVUar4JZvt4fG1C');
-define('PAYPAL_SECRET', 'EAp7C3ss-0EWBS6qcyta1q5H2eBzxvciQR1igoduOEB52S1LmLfVwzqddoObnm6hMJkoB50c6kRWEo6k');
+define('PAYPAL_CLIENT_ID', 'AUqlxS2v78zaPeBrh0gH2DIEzgdRnmNIcvcP2WC4mFgtMSwYNBA_6gZdUXF7fdJPrBFtuw2He1HB6tki');
+define('PAYPAL_SECRET', 'EDGGP00RcN1meTiaLZuq4i7mVgTanOStjmBcBiH1AeCdQIYgyyybTcI_tTp9UXCBqimgMtSOBu5oPFKs');
 define('PAYPAL_ENVIRONMENT', 'sandbox');
 ?>
